58b847fa33c9c400428880c73a400be368238184
Vanamman <vanamman06@gmail.com>
2017-04-28 21:13:10 -0500

    updated mpv.py wording

--- mpv.py
+++ mpv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-#Ask user for link to youtube video, then streams that video in the best quality using the mpv video player
+#Ask user for link to a video, then streams that video in the best quality using the mpv video player
 
 import subprocess
 
-youtube = input("Please enter a youtube link: ")
+youtube = input("Please enter a video link: ")
 quality = "--ytdl-format=bestvideo+bestaudio"
 
 subprocess.call([r"/usr/bin/mpv", youtube, quality])
